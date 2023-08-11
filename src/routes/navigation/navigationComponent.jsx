7d1971a5f42d94af909f956ddcb128026b44f46f
Adarsh Shenoy <adarshrsedu@gmail.com>
2023-08-11 20:23:59 +0530

    Added basic routing and navbar

---
+++ src/routes/navigation/navigationComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import { Link, Outlet } from 'react-router-dom';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return(
+        <Fragment>
+          <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <h1>Zelda</h1>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>Shop</Link>
+            </div>
+          </div>
+          <Outlet/>
+        </Fragment>
+      )
+}
+
+export default Navigation
