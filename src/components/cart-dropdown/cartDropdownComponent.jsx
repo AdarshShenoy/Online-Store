397ffc74ddd88a121c0aaf889109c8d0509ea58f
Adarsh Shenoy <adarshrsedu@gmail.com>
2023-08-22 15:51:35 +0530

    Created context, functionality for checkout items

--- src/components/cart-dropdown/cartDropdownComponent.jsx
+++ src/components/cart-dropdown/cartDropdownComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cartContext';
 import './cartDropdownStyles.scss';
 import Button from '../button/buttonComponent';
@@ -7,6 +8,12 @@ import CartItem from '../cart-item/cartItemComponent';
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+      navigate('/checkout');
+    }
+
   return (
     <div className='cart-dropdown-container'>
         <div className='cart-items'>
@@ -14,7 +21,7 @@ const CartDropdown = () => {
             <CartItem key = {item.id} cartItem={item}/>
             ))}
         </div>
-        <Button>Checkout</Button>
+        <Button onClick = {goToCheckoutHandler}>Checkout</Button>
         
     </div>
   )
